2f78438378868b8f5c52a0759686ee2914bb6d1d
Eren K <thgeren@gmail.com>
2026-06-29 23:54:23 +0200

    Issue 1#: Erstellen von Unit Test

---
+++ test/MainTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void checkForEvenNegativeNum() {
+        assertTrue(Main.isNumberEven(-4));
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkForUnevenNegativeNum() {
+        assertFalse(Main.isNumberEven(-3));
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkForUnevenPositiveNum() {
+        assertFalse(Main.isNumberEven(1));
+    }
+
+    @org.junit.jupiter.api.Test
+    void checkForEvenPositiveNum() {
+        assertFalse(Main.isNumberEven(2));
+    }
+}
